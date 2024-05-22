7cfed6e1b437525c5bd30160a98f9bad05578095
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-22 23:40:12 +0600

    custom not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Not Found</h1>
+      <p>This page does not exist</p>
+    </div>
+  );
+}
---
+++ src/app/products/[productId]/reviews/[reviewsId]/not-found.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Not Found</h1>
+      <p>Review does not exist</p>
+    </div>
+  );
+}
