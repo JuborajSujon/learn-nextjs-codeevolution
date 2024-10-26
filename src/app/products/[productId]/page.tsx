77e5cef14d320a84187b13e0287c5e2bda2668ec
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-26 20:16:37 +0600

    understand dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,5 @@
+const ProductDetails = ({ params }: { params: { productId: string } }) => {
+  return <h2>Product Details {params.productId}</h2>;
+};
+
+export default ProductDetails;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h2 className="text-2xl text-violet-500">Product List</h2>
+      <h3>Prouct 1</h3>
+      <h3>Prouct 2</h3>
+      <h3>Prouct 3</h3>
+    </div>
+  );
+};
+
+export default ProductList;
