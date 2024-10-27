0118fa35a60eb249a8acdcb17026c499e4512e25
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-27 20:17:17 +0600

    recoving from errors

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { notFound } from "next/navigation";
 
-const ReviewDetail = async ({
+function getRandomInt(count: number): number {
+  return Math.floor(Math.random() * count);
+}
+
+const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  const random = getRandomInt(2);
+  if (random === 0) {
+    throw new Error("Something went wrong");
+  }
   if (parseInt(params.reviewId) > 1000) return notFound();
   return (
     <h2>
