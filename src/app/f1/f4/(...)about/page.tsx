3fef98491e0bf0664568936ca8b14cab6c88c539
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-27 22:29:24 +0600

    learn about intercepting routes

---
+++ src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "About codeevolution",
+};
+const InterceptedAbout = () => {
+  return <h1>(...) Intercepted About page</h1>;
+};
+
+export default InterceptedAbout;
