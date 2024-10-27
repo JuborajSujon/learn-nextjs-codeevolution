f4f6076686f097917be33dd86090e9b253e33730
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-27 08:25:49 +0600

    understand metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-const ProductDetails = async ({
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) => {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+const ProductDetails = async ({ params }: Props) => {
   return <h2>Product Details {params.productId}</h2>;
 };
 
