c8e2a4895f9182fd5535abb23df10cb834719304
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-28 23:17:49 +0600

    understand layout

--- routes-demo-self/src/app/layout.tsx
+++ routes-demo-self/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className="bg-slate-800 text-slate-50 py-3">
+          <nav className="flex justify-center items-center gap-2">
+            <Link href="/">Home</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/about">About</Link>
+            <Link href="/blog">Blog</Link>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer className="bg-slate-800 text-slate-50 py-2">Footer</footer>
+      </body>
     </html>
   );
 }
