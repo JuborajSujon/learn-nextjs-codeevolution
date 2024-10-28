d1c9529f4703255792c3d3ad647407a2d8305ce7
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-28 13:59:44 +0600

    file colocation

---
+++ routes-demo-self/src/app/graph/line-chart.tsx
@@ -0,0 +1,3 @@
+export default function LineChart() {
+  return <h1 className="text-3xl font-bold">Line Chart</h1>;
+}
---
+++ routes-demo-self/src/app/graph/page.tsx
@@ -0,0 +1,7 @@
+function Barchar() {
+  return <h1 className="text-3xl font-bold">Line Chart</h1>;
+}
+
+export default function Graph() {
+  return <h1 className="text-3xl font-bold">Graph</h1>;
+}
