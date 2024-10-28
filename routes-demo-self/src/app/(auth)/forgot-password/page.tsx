483c0c95e932e020f7f9581bbe7dd9cbef0e523b
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-28 14:28:25 +0600

    routes group

---
+++ routes-demo-self/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ForgotPassword() {
+  return <h1 className="text-3xl font-bold">Forgot Password Page</h1>;
+}
---
+++ routes-demo-self/src/app/(marketing)/mabout/page.tsx
@@ -0,0 +1,3 @@
+export default function MarketingAbout() {
+  return <h1 className="text-3xl font-bold">Marketing About page</h1>;
+}
