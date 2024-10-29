dd69d81acc86f11c422d5c1ed2a832cf7872d811
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-29 10:27:35 +0600

    Routing Metadata

--- routes-demo-self/src/app/about/page.tsx
+++ routes-demo-self/src/app/about/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+  title: "About page",
+};
 export default function About() {
   return <h1 className="text-3xl font-bold">About me</h1>;
 }
--- routes-demo-self/src/app/products/[productId]/page.tsx
+++ routes-demo-self/src/app/products/[productId]/page.tsx
@@ -11,12 +11,23 @@
 //     </div>
 //   );
 // }
-
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 100);
+  });
+  return { title: `Product ${title}` };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const { productId } = await params;
   return (
     <div className="p-10">
