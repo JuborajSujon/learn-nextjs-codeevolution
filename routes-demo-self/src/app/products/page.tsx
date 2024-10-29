70731a723e6a110ab2ff99c89be681ee9d7c0537
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-29 18:05:15 +0600

    link component navigation

--- routes-demo-self/src/app/products/page.tsx
+++ routes-demo-self/src/app/products/page.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import Link from "next/link";
 
 export default function Products() {
+  const productId = 100;
   return (
     <div className="p-10">
       <h2 className="text-3xl font-bold">Products Lists</h2>
       <ul className="text-lg list-disc">
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link className="" href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="" href="/products/1">
+            Product 1
+          </Link>
+        </li>
+        <li>
+          <Link className="" href="/products/2">
+            Product 2
+          </Link>
+        </li>
+        <li>
+          <Link className="" href="/products/3" replace>
+            Product 3
+          </Link>
+        </li>
+        <li>
+          <Link className="" href={`/products/${productId}`}>
+            Product {productId}
+          </Link>
+        </li>
       </ul>
     </div>
   );
